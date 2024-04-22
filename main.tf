4eb85a4f5b8a0b86df47c5336389824165c821c2
Edward Ritter <esritter@gmail.com>
2024-04-22 17:30:55 -0400

    another fix autoscaling

--- main.tf
+++ main.tf
@@ -63,11 +63,24 @@ module "blog_alb" {
     default = {
       port     = 80
       protocol = "HTTP"
-
+      forward =  {
+        target_group_key = "instance"
+      }
     }
   }
 
+  target_groups = {
+    instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
 
+      protocol_version = "HTTP1"
+
+      port = 80
+    }
+  }
 
   tags = {
     Environment = "dev"
